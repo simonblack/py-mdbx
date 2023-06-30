77ce5721b9f74f4dae3da08da01ee304a12b7fb6
Simon Black <mail@simon.black>
2023-06-30 03:02:33 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-mdbx",
+    version="0.0.0",
+    author="Simon Black",
+    url="https://github.com/simonblack/py-mdbx",
+    email="mail@simon.black",
+    description="Python binsings for libmdbx",
+    packages=find_packages(),
+)
